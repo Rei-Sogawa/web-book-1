c6974c4197d762fc80040cdf77f2779248c6d28c
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-10-03 12:33:20 +0900

    a

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,11 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
 
-import { Book, Chapter } from "shared";
+import { Book, Chapter } from 'shared'
+
+const TOKYO = 'asia-northeast1'
+const functionsWithRegion = functions.region(TOKYO)
+
+export const hello = functionsWithRegion.https.onRequest((req, res) => {
+  res.status(200).send('Hello')
+})
